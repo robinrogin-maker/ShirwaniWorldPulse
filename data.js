96233a8a7087cee000e33a69ef9c3fc57c1d6869
robinrogin-maker <robinrogin@gmail.com>
2026-09-19 00:42:12 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1401,5 +1401,7 @@ const DEFAULT_MUSIC_FAVS = [
   { title: "Demis Roussos Music MegaMix", url: "https://www.youtube.com/watch?v=F8sg7Qdp-tI" },
   { title: "Boney M. Greatest Hits Full Album", url: "https://www.youtube.com/watch?v=AClOh_Aznxk" },
   { title: "Flamenco Cha-Cha in Dubai - Exclusive Desert Dance Show", url: "https://www.youtube.com/watch?v=pCTGJleyNrs" },
+   { title: "Bad Bunny | Afro House mix | NUEVAYoL, DTMF, EoO, PIToRRO DE COCO | DJ set | Steve Cortez", url: "https://www.youtube.com/watch?v=RyUx1sztt_I" },
+   { title: "Bad Bunny Mix 2025 | BEST OF BAD BUNNY REGGAETON MIX", url: "https://www.youtube.com/watch?v=0GDs7j54nHU" },
   
 ];
