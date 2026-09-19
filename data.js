ec7cc1285311f921f68e42b868ee2cb88c3f4b86
robinrogin-maker <robinrogin@gmail.com>
2026-09-19 19:05:10 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1404,5 +1404,6 @@ const DEFAULT_MUSIC_FAVS = [
   { title: "Bad Bunny Mix 2025 | BEST OF BAD BUNNY REGGAETON MIX", url: "https://www.youtube.com/watch?v=0GDs7j54nHU" }, 
   { title: "Cuban Music Mix 🎵 Buena Vista , Cha Cha & Latin Jazz - Rhythms of Havana", url: "https://www.youtube.com/watch?v=y8pPKZTIKLQ&list=RDy8pPKZTIKLQ&start_radio=1" },
   { title: "BAD BUNNY MIX 2026 - 25 Las mejores canciones de Bad Bunny 2026 - BAD BUNNY BEST HITS IN 2026", url: "https://www.youtube.com/watch?v=PboonSTC1xE&list=RDPboonSTC1xE&start_radio=1" },
+  { title: "🌴 Tropical Sunset Reggae 🌊 Hawaiian Beach Vibes & Endless Summer Chill", url: "https://www.youtube.com/watch?v=cPeJGZPVipk&list=RDcPeJGZPVipk&start_radio=1" },
   
 ];
